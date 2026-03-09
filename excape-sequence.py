e6d5287005c63135f6cc84b354d4e63b7eb9cafa
Josh <joshuaoladipo2005@gmail.com>
2026-03-09 14:31:19 +0100

    how to break line or new line

--- excape-sequence.py
+++ excape-sequence.py
@@ -4,4 +4,14 @@ print('Hi "python"')
 #how to add a real slash \
 print("path: c: \\users \\baraa")
 
-#how to break line
+#how to break line or new line
+    #method 1
+print('message 1')
+print()
+print('message 2')
+    #method 2
+print('message 1\n\n\n')
+print('message 2')
+
+print('message1\n\n\nmessage2')
+
