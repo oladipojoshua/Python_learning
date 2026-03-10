9b5c0af5f8dd916e1bd73d6b2ada58b5820edaa8
Josh <joshuaoladipo2005@gmail.com>
2026-03-10 15:25:36 +0100

    2nd challenge

---
+++ Variable-challenge.py
@@ -0,0 +1,9 @@
+#Here is the text from the image
+#info@datawithbaraa.com
+#support@datawithbaraa.com
+#www.datawithbaraa.com
+
+mail ="datawithbaraa.com"
+print(f'info@{mail}')
+print(f"support@{mail}")
+print(f"www.@{mail}")
