466d972d5535633f98df2237ef886fbf0c71186d
Josh <joshuaoladipo2005@gmail.com>
2026-03-10 15:44:44 +0100

    3rd challenge about data types

---
+++ data_type_challenge.py
@@ -0,0 +1,25 @@
+# Create 5 variables - each with a different data type:
+# 1. Your age
+# 2. Your height (with decimals)
+# 3. Your name
+# 4. Are you a student?
+# 5. Something with no value yet
+
+# Then print the values, data types, lengths of all variables
+Age = 23
+height = 42.3
+Name = "Joshua"
+student = True
+no_value = ""
+
+print(type(Age))
+print(type(height))
+print(type(Name))
+print(type(student))
+print(type(no_value))
+
+print(len(Name))
+#print(len(height)) #obj. does not have length
+#print(len(Age)) #obj. does not have length
+#print(len(student)) #obj. does not have length
+print(len(no_value))
